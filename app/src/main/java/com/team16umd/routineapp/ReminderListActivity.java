4cc86e8ca0a4b0b176602f1fd68288692deeab57
bend87 <m@myersmatthew.com>
2016-04-27 23:26:16 -0400

    added ToDo where I get a NullPointerException

--- app/src/main/java/com/team16umd/routineapp/ReminderListActivity.java
+++ app/src/main/java/com/team16umd/routineapp/ReminderListActivity.java
@@ -165,6 +165,7 @@ public class ReminderListActivity extends ListActivity {
             mUserRef.child("points").addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
+                    //TODO- I get a NullPointerException for toString() here after connecting to FB
                     mPoints.setText(dataSnapshot.getValue().toString());
                 }
 
